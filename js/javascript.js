2e1feab86c577d269d7de175cad03e92cb1cd05c
Corey Fulop <corey.fulop@gmail.com>
2023-09-18 11:40:40 +0100

    Create addBookToLibrary function

--- js/javascript.js
+++ js/javascript.js
@@ -13,6 +13,14 @@ Book.prototype.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 }
 
-function addBookToLibrary() {
+const bookCardContainer = document.querySelector(".card-container");
 
+function addBookToLibrary() {
+    let newTitle = prompt("Enter new book title:");
+    let newAuthor = prompt("Enter the author:");
+    let newPages = prompt("Enter the number of pages:");
+    let newRead = prompt("Have you read this book:");
+    let newBook = new Book(newTitle, newAuthor, newPages, newRead);
+    myLibrary.push(newBook);
 }
+
